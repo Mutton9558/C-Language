9c4ea4fc674479d1d0d822144ae6edce2ee00b67
Mutton9558 <69904687+Mutton9558@users.noreply.github.com>
2024-09-10 20:25:57 +0800

    Made a file to remember useful C basic syntaxs and other key aspects

---
+++ C/C_Basics.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int age = 3; // int variable declaration
+    char str[] = "Johnathan"; // string variable declaration
+    char character = 'd'; // char variable declaration
+
+    // print
+    printf("Hello World!\n");
+    // format specifiers %d for int, %c for char, %s for string and %f for floating point
+    printf("I am %d years old\n", age);
+    printf("I am %s\n", str);
+    return 0;
+}
